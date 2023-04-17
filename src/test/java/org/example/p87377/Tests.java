9c7d933737118c3ba31af6558e8c503a273c0f98
jhs512 <jangka512@gmail.com>
2023-04-17 13:10:35 +0900

    실패하는 테스트 구현

---
+++ src/test/java/org/example/p87377/Tests.java
@@ -0,0 +1,21 @@
+package org.example.p87377;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Tests {
+    @Test
+    @DisplayName("교점, [[1, -1, 0], [2, -1, 0]]")
+    void t1() {
+        assertThat(
+                new Solution().intersection(
+                        new int[]{1, -1, 0},
+                        new int[]{2, -1, 0}
+                )
+        ).isEqualTo(
+                new int[]{0, 0}
+        );
+    }
+}
