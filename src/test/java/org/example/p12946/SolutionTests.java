32019813fd3adcb2b54a55a493521e1a57bc26e5
jhs512 <jangka512@gmail.com>
2023-04-19 11:24:57 +0900

    N=2 인경우

--- src/test/java/org/example/p12946/SolutionTests.java
+++ src/test/java/org/example/p12946/SolutionTests.java
@@ -28,4 +28,24 @@ public class SolutionTests {
                 new int[][]{{1, 2}, {1, 3}, {2, 3}}
         );
     }
+
+    @Test
+    @DisplayName("from=1, to=2, n=2 => [[1,3], [1,2], [3,2]]")
+    void t03() {
+        assertThat(
+                new Hanoi(1, 2, 2).toArray()
+        ).isEqualTo(
+                new int[][]{{1, 3}, {1, 2}, {3, 2}}
+        );
+    }
+
+    @Test
+    @DisplayName("from=2, to=1, n=2 => [[2,3], [2,1], [3,1]]")
+    void t04() {
+        assertThat(
+                new Hanoi(2, 1, 2).toArray()
+        ).isEqualTo(
+                new int[][]{{2, 3}, {2, 1}, {3, 1}}
+        );
+    }
 }
