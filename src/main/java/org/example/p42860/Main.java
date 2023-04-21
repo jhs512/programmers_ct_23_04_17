d8a796f0f6fbb7b99cbaa7af159ea2aa302bfb51
jhs512 <jangka512@gmail.com>
2023-04-21 15:39:07 +0900

    조이스틱 시작

---
+++ src/main/java/org/example/p42860/Main.java
@@ -0,0 +1,12 @@
+package org.example.p42860;
+
+public class Main {
+}
+
+class Solution {
+    public static boolean isDebug = false;
+
+    public int solution(String name) {
+        return 1;
+    }
+}
